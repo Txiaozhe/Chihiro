9823e7f14be7ca868928940ce861dca5df052a20
Txiaozhe <txiaozhe@gmail.com>
2020-06-06 21:23:45 +0800

    add.

--- blog-hexo/build.sh
+++ blog-hexo/build.sh
@@ -2,5 +2,5 @@
 npm run clean
 npm run build
 
-cp -r ./public/* /Users/tangxiaoji/GitHub/txiaozhe.github.io
+cp -r ./public/* ~/GitHub/txiaozhe.github.io
 echo 'build & copy success! Go to public use GitHub for txiaozhe.github.io'
